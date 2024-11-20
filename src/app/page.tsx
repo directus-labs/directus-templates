32bb3a8b39b50aeb473b3e91e38e9533e13b0364
Lindsey Zylstra <lindsey@directus.io>
2024-11-20 14:02:30 -0800

    Initial commit with boilerplate

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+
+/**
+ * The main page component that renders the HomePage component.
+ *
+ * @returns {JSX.Element} The rendered HomePage component.
+ */
+const Page = () => {
+    return <></> ;
+};
+
+export default Page;
