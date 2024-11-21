c5133549df5c4c4f97759ed5aacec635acfe5e8a
Lindsey Zylstra <lindsey@directus.io>
2024-11-21 13:59:30 -0800

    Reorganize structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-
-
 /**
  * The main page component that renders the HomePage component.
  *
  * @returns {JSX.Element} The rendered HomePage component.
  */
-const Page = () => {
-    return <></> ;
+const HomePage = () => {
+  return (
+    <div className="container mx-auto py-8">
+      <h1 className="text-2xl font-bold mb-4">Home Page</h1>
+      <p>This will be the home page.</p>
+    </div>
+  );
 };
 
-export default Page;
+export default HomePage;
