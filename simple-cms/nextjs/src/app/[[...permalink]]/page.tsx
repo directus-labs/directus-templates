686f137df859f4e07fe5cec7bd015a19ab897521
Lindsey Zylstra <lindsey@directus.io>
2024-12-03 14:04:39 -0800

    Fix for params async error

--- simple-cms/nextjs/src/app/[[...permalink]]/page.tsx
+++ simple-cms/nextjs/src/app/[[...permalink]]/page.tsx
@@ -2,16 +2,17 @@ import { useDirectus } from '@/lib/directus/directus';
 import PageBuilder from '@/components/PageBuilder';
 import { PageBlock } from '@/types/directus-schema';
 
-export default async function HomePage() {
-	const { directus, readItems } = useDirectus();
+export default async function Page({ params }: { params: Promise<{ permalink?: string[] }> }) {
+	const { permalink } = await params;
 
-	const pageFilter = {
-		permalink: { _eq: '/' },
-	};
+	const permalinkSegments = permalink || [];
+	const resolvedPermalink = `/${permalinkSegments.join('/')}`.replace(/\/$/, '') || '/';
+
+	const { directus, readItems } = useDirectus();
 
 	const pageData = await directus.request(
 		readItems('pages', {
-			filter: pageFilter,
+			filter: { permalink: { _eq: resolvedPermalink } },
 			limit: 1,
 			fields: ['title', { blocks: ['id', 'background', 'collection', 'item', 'sort'] }],
 			deep: {
@@ -25,7 +26,7 @@ export default async function HomePage() {
 	const page = pageData[0];
 
 	if (!page || !Array.isArray(page.blocks)) {
-		return <div>404 - Page Not Found</div>;
+		return <div className="text-center text-xl mt-[20%]">404 - Page Not Found</div>;
 	}
 
 	const blocks: PageBlock[] = page.blocks.filter((block): block is PageBlock => typeof block !== 'string' && !!block);
