17250849446359aeb14a94e2aa0b47072cfb5a25
Lindsey Zylstra <lindsey@directus.io>
2024-12-05 13:50:18 -0800

    Page SEO

--- simple-cms/nextjs/src/app/[[...permalink]]/page.tsx
+++ simple-cms/nextjs/src/app/[[...permalink]]/page.tsx
@@ -1,6 +1,7 @@
 import { useDirectus } from '@/lib/directus/directus';
 import PageBuilder from '@/components/PageBuilder';
 import { PageBlock } from '@/types/directus-schema';
+import Head from 'next/head';
 
 export default async function Page({ params }: { params: Promise<{ permalink?: string[] }> }) {
 	const { permalink } = await params;
@@ -14,7 +15,7 @@ export default async function Page({ params }: { params: Promise<{ permalink?: s
 		readItems('pages', {
 			filter: { permalink: { _eq: resolvedPermalink } },
 			limit: 1,
-			fields: ['title', { blocks: ['id', 'background', 'collection', 'item', 'sort'] }],
+			fields: ['title', 'description', { blocks: ['id', 'background', 'collection', 'item', 'sort'] }],
 			deep: {
 				blocks: {
 					_sort: ['sort'],
@@ -38,5 +39,27 @@ export default async function Page({ params }: { params: Promise<{ permalink?: s
 		},
 	];
 
-	return <PageBuilder sections={sections} />;
+	const title = page.title;
+	const metaDescription = page.description;
+	const canonicalUrl = `${process.env.NEXT_PUBLIC_SITE_URL}${resolvedPermalink}`;
+
+	return (
+		<>
+			{/* SEO Metadata */}
+			<Head>
+				<title>{title}</title>
+				{metaDescription && <meta name="description" content={metaDescription} />}
+				<link rel="canonical" href={canonicalUrl} />
+
+				{/* Basic Open Graph Metadata */}
+				<meta property="og:title" content={title} />
+				{metaDescription && <meta property="og:description" content={metaDescription} />}
+				<meta property="og:url" content={canonicalUrl} />
+				<meta property="og:type" content="website" />
+			</Head>
+
+			{/* Page Content */}
+			<PageBuilder sections={sections} />
+		</>
+	);
 }
