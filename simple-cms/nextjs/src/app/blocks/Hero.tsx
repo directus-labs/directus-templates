3c1e5eec0810c2e5631fb54378e1397881e2c2c5
Lindsey Zylstra <lindsey@directus.io>
2024-12-05 14:44:38 -0800

    Hero updated to match design

--- simple-cms/nextjs/src/app/blocks/Hero.tsx
+++ simple-cms/nextjs/src/app/blocks/Hero.tsx
@@ -3,6 +3,7 @@ import BaseText from '@/components/Text';
 import BlockButtonGroup from './ButtonGroup';
 import Title from '@/components/Title';
 import Headline from '@/components/Headline';
+import DirectusImage from '@/components/DirectusImage';
 
 interface BlockHeroProps {
 	uuid: string;
@@ -21,36 +22,38 @@ const BlockHero = async ({ uuid }: BlockHeroProps) => {
 
 	const { alignment, title, headline, description, image, button_group } = block;
 
-	const backgroundImageUrl = image ? `${process.env.NEXT_PUBLIC_DIRECTUS_URL}/assets/${image}` : null;
-
 	return (
 		<section
-			className={`relative flex flex-col items-center justify-center w-screen min-h-[60vh]`}
-			style={{
-				backgroundImage: backgroundImageUrl
-					? `linear-gradient(
-              var(--gradient-overlay),
-              var(--gradient-overlay)
-            ), url('${backgroundImageUrl}')`
-					: undefined,
-				backgroundSize: 'cover',
-				backgroundPosition: 'center',
-			}}
+			className={`relative w-full items-center min-h-[60vh] flex flex-col ${
+				alignment === 'center' ? 'items-center text-center' : 'md:flex-row'
+			} ${alignment === 'right' ? 'md:flex-row-reverse' : ''} gap-6`}
 		>
-			<div className={`max-w-4xl ${alignment ? `text-${alignment}` : 'text-left'}`}>
+			<div
+				className={`w-full ${
+					alignment === 'center' ? 'md:w-1/2' : 'md:w-1/2 text-left'
+				} flex flex-col justify-center gap-4`}
+			>
 				<Title title={title} />
 				<Headline headline={headline} />
-
 				{description && (
-					<BaseText content={description} align={alignment ?? 'left'} className="text-lg text-foreground mb-8" />
+					<BaseText
+						content={description}
+						align={alignment === 'center' ? 'center' : 'left'}
+						className="text-lg text-foreground"
+					/>
 				)}
-
 				{button_group?.id && (
 					<div className={`mt-6 ${alignment === 'center' ? 'flex justify-center' : ''}`}>
 						<BlockButtonGroup uuid={button_group.id} />
 					</div>
 				)}
 			</div>
+
+			{image && (
+				<div className={`w-full ${alignment === 'center' ? 'md:max-w-2xl mx-auto' : 'md:w-1/2'} relative`}>
+					<DirectusImage uuid={image as string} alt={title || 'Hero Image'} className="rounded-lg" />
+				</div>
+			)}
 		</section>
 	);
 };
