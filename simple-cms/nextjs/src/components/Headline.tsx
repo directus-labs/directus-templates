f6694761592b85f064a4d036ff913d5db708a653
Lindsey Zylstra <lindsey@directus.io>
2024-12-12 14:57:56 -0800

    Headline and title responsive

--- simple-cms/nextjs/src/components/Headline.tsx
+++ simple-cms/nextjs/src/components/Headline.tsx
@@ -1,12 +1,20 @@
 interface HeadlineProps {
 	headline?: string | null;
 	className?: string;
+	as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'div';
 }
 
-const BaseHeadline = ({ headline, className = '' }: HeadlineProps) => {
+const BaseHeadline = ({ headline, className = '', as: Component = 'p' }: HeadlineProps) => {
 	if (!headline) return null;
 
-	return <p className={`text-headline font-heading text-foreground font-normal ${className}`}>{headline}</p>;
+	return (
+		<Component
+			className={`font-heading text-foreground font-normal ${className}
+         text-4xl md:text-5xl lg:text-headline`}
+		>
+			{headline}
+		</Component>
+	);
 };
 
 export default BaseHeadline;
--- simple-cms/nextjs/src/components/Title.tsx
+++ simple-cms/nextjs/src/components/Title.tsx
@@ -1,12 +1,20 @@
 interface TitleProps {
 	title?: string | null;
 	className?: string;
+	as?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p' | 'span' | 'div';
 }
 
-const BaseTitle = ({ title, className = '' }: TitleProps) => {
+const BaseTitle = ({ title, className = '', as: Component = 'h2' }: TitleProps) => {
 	if (!title) return null;
 
-	return <h2 className={`text-title font-heading text-accent font-normal uppercase ${className}`}>{title}</h2>;
+	return (
+		<Component
+			className={`font-heading text-accent font-normal uppercase ${className}
+         text-lg md:text-xl lg:text-title`}
+		>
+			{title}
+		</Component>
+	);
 };
 
 export default BaseTitle;
