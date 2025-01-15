451c082db7bb174b9c21741bf3e33a95323cff59
Lindsey Zylstra <lindseyzylstra@gmail.com>
2025-01-15 11:36:14 -0800

    Form submission id optional

--- simple-cms/nextjs/src/types/directus-schema.ts
+++ simple-cms/nextjs/src/types/directus-schema.ts
@@ -194,8 +194,7 @@ export interface FormSubmission {
 }
 
 export interface FormSubmissionValue {
-	/** @required */
-	id: string;
+	id?: string;
 	/** @description Parent form submission for this value. */
 	form_submission?: FormSubmission | string | null;
 	field?: FormField | string | null;
